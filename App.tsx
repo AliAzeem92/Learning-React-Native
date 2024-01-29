71f324ab88dca2d6105a1a437147743e1c460298
Ali Azeem <aliazeemaliazeem786@gmail.com>
2024-01-29 23:43:01 +0500

    Call

--- App.tsx
+++ App.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import {Text, StyleSheet, View, Button} from 'react-native';
 
-function App() {
+let name = 'Ali Azeem';
+var age = '19';
+const rollNum = 'BSCS';
+
+const App = () => {
   return (
     <View>
-      <Text style={{fontSize: 40}}>Hello World</Text>
-      <Text style={{fontSize: 30}}>Hello World</Text>
-      <Text style={{fontSize: 20}}>Hello World</Text>
-      <Text style={{fontSize: 10}}>Hello World</Text>
-      <Button title="Press Here"></Button>
-      <Button title="Press Me"></Button>
+      <Text style={{fontSize: 50, color: 'purple'}}>Hello World</Text>
+      <Text style={{fontSize: 40, color: 'Red'}}> {name} </Text>
+      <Text style={{fontSize: 30, color: 'Blue'}}> {age} </Text>
+      <Text style={{fontSize: 20, color: 'black'}}> {rollNum} </Text>
     </View>
   );
-}
+};
 
 export default App;
