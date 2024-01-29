0e64230c396f5f520a6c8be4b269a1a3cfa456f0
Ali Azeem <aliazeemaliazeem786@gmail.com>
2024-01-29 23:53:01 +0500

    Nested Compoments

--- App.tsx
+++ App.tsx
@@ -15,6 +15,7 @@ const App = () => {
 const UserData = () => {
   return (
     <View>
+      <ClassData />
       <Text style={{fontSize: 30, color: 'red'}}>Name: Ali Azeem</Text>
       <Text style={{fontSize: 30, color: 'red'}}>Age: 19</Text>
       <Text style={{fontSize: 30, color: 'red'}}>Class: BSCS</Text>
