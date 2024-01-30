375f0551e412087da657d3308e79e689214f18a2
Ali Azeem <aliazeemaliazeem786@gmail.com>
2024-01-30 22:41:18 +0500

    External CSS

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Button, Text, View} from 'react-native';
+import {Button, Text, View, StyleSheet} from 'react-native';
 
 const App = () => {
   return (
@@ -13,8 +13,31 @@ const App = () => {
         }}>
         Styling
       </Text>
+      <Text style={style.textBox}>External CSS</Text>
+      <Text style={style.textBox}>External CSS</Text>
+      <Text style={style.textBox}>External CSS</Text>
+      <Text style={style.textBox}>External CSS</Text>
+      <Text style={style.textBox}>External CSS</Text>
+      <Text style={style.textBox}>External CSS</Text>
     </View>
   );
 };
+
+const style = StyleSheet.create({
+  textBox: {
+    fontSize: 30,
+    color: 'blue',
+    backgroundColor: 'gold',
+    padding: 10,
+    textAlign: 'center',
+    marginTop: 10,
+    marginBottom: 10,
+    marginLeft: 10,
+    marginRight: 10,
+    borderRadius: 10,
+    borderWidth: 5,
+    borderColor: 'gray',
+  },
+});
 
 export default App;
