5a8f1643bae02a82a81e09d5087628907e126ebe
Ali Azeem <aliazeemaliazeem786@gmail.com>
2024-01-30 22:27:36 +0500

    In-Line CSS

--- App.tsx
+++ App.tsx
@@ -1,72 +1,17 @@
-// import React, {useState} from 'react';
-// import {Button, Text, View} from 'react-native';
-
-// const App = () => {
-//   return (
-//     <View>
-//       <Text
-//         style={{
-//           fontSize: 50,
-//           backgroundColor: 'pink',
-//           color: 'blue',
-//           textAlign: 'center',
-//         }}>
-//         Props
-//       </Text>
-//       <userName />
-//     </View>
-//   );
-
-//   const userName = () => {
-//     return (
-//       <View>
-//         <Text
-//           style={{
-//             fontSize: 50,
-//             backgroundColor: 'pink',
-//             color: 'blue',
-//             textAlign: 'center',
-//           }}>
-//           Props
-//         </Text>
-//       </View>
-//     );
-//   };
-// };
-
-// export default App;
-
 import React, {useState} from 'react';
 import {Button, Text, View} from 'react-native';
 
 const App = () => {
-  const [fatherName, sonName] = useState('Aamir Azeem');
-
   return (
     <View>
       <Text
         style={{
-          fontSize: 50,
-          backgroundColor: 'pink',
-          color: 'blue',
           textAlign: 'center',
-        }}>
-        Props
-      </Text>
-      <UserName name={fatherName} />
-      <Button title="Child Name" onPress={() => sonName('Ali Azeem')} />
-    </View>
-  );
-};
-const UserName = (props: any) => {
-  return (
-    <View>
-      <Text
-        style={{
           fontSize: 30,
+          backgroundColor: 'lightblue',
           color: 'red',
         }}>
-        {props.name}
+        Styling
       </Text>
     </View>
   );
