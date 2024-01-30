c106d0f92a5436025dda35e6fb674f8d9b2abea3
Ali Azeem <aliazeemaliazeem786@gmail.com>
2024-01-30 21:27:52 +0500

    Idea about State

--- App.tsx
+++ App.tsx
@@ -1,23 +1,63 @@
-import React from 'react';
+// import React, {useState} from 'react';
+// import {Button, Text, View} from 'react-native';
+
+// const App = () => {
+//   const [first, second] = useState('Ali');
+
+//   function sec() {
+//     second('Azeem');
+//   }
+
+//   return (
+//     <View>
+//       <Text style={{fontSize: 50, color: 'blue', textAlign: 'center'}}>
+//         State
+//       </Text>
+//       <Text style={{fontSize: 30, color: 'red'}}>{first} </Text>
+//       <Button title="Second Name" onPress={sec} />
+//     </View>
+//   );
+// };
+
+// export default App;
+// import React, {useState} from 'react';
+// import {Button, Text, View} from 'react-native';
+
+// const App = () => {
+//   const [father, son] = useState('Aamir');
+//   function ofspring() {
+//     son('Ali');
+//   }
+
+//   return (
+//     <View>
+//       <Text style={{fontSize: 50, color: 'blue', textAlign: 'center'}}>
+//         State
+//       </Text>
+//       <Text style={{fontSize: 30, color: 'red'}}>{father}</Text>
+//       <Button title="Change Name" onPress={ofspring} />
+//     </View>
+//   );
+// };
+
+// export default App;
+
+import React, {useState} from 'react';
 import {Button, Text, View} from 'react-native';
 
 const App = () => {
-  const fruit = (value: any) => {
-    console.warn(value);
-  };
+  const [city1, city2] = useState('Karachi');
+  function second() {
+    city2('Islamabad');
+  }
 
   return (
     <View>
-      <Text style={{fontSize: 50, color: 'blue'}}>Buttons</Text>
-      {/* With Params */}
-      <Button
-        title="On Press"
-        color={'lightgreen'}
-        onPress={() => fruit('Hello')}
-      />
-      <Text></Text>
-      {/* Without Params */}
-      <Button title="On Press 2" color={'green'} onPress={fruit} />
+      <Text style={{fontSize: 50, color: 'blue', textAlign: 'center'}}>
+        State
+      </Text>
+      <Text style={{fontSize: 30, color: 'gold'}}>{city1}</Text>
+      <Button title="Second City" onPress={second} />
     </View>
   );
 };
