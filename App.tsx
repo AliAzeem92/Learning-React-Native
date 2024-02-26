d6f991c9f2d20506b7786200d4e85e2d966352ae
Ali Azeem <aliazeemaliazeem786@gmail.com>
2024-02-26 17:00:23 +0500

    Simple Form

--- App.tsx
+++ App.tsx
@@ -1,44 +1,81 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
 import Header from './components/Header';
 
 const App = () => {
-  const [name,useName] = useState("");
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+
+  const [display, setDisplay] = useState(false);
+  const resetDetails = () => {
+    setName('');
+    setPassword('');
+    setEmail('');
+    setDisplay(false);
+  };
 
   return (
     <View>
       <Header />
-      <Text style={styles.heading} >Hadling Text Input</Text>
-      <Text style={styles.changeHeading} >Your Name is: {name} </Text>
+      <Text style={styles.heading}>Simple Form</Text>
       <TextInput
-      style={styles.textInput}
-      placeholder='Your Name '
-      value={name}
-      onChangeText={(text)=>useName(text)}
+        style={styles.textInput}
+        placeholder="Enter Username "
+        onChangeText={text => setName(text)}
+        value={name}
       />
-      <Button title='Clear Input Box' onPress={()=>{useName('')}} />
+      <TextInput
+        style={styles.textInput}
+        placeholder="Enter Password"
+        onChangeText={text => setPassword(text)}
+        secureTextEntry={true}
+        value={password}
+      />
+      <TextInput
+        style={styles.textInput}
+        placeholder="Enter Email"
+        onChangeText={text => setEmail(text)}
+        value={email}
+      />
+      <View style={{margin: 10}}>
+        <Button
+          color={'green'}
+          title="Print Details"
+          onPress={() => setDisplay(true)}
+        />
+      </View>
+      <View style={{marginTop: -5, margin: 10}}>
+        <Button title="Clear Details" onPress={resetDetails} />
+      </View>
+      <View>
+        {display ? (
+          <>
+            <Text style={{fontSize: 25}}>Your name is: {name}</Text>
+            <Text style={{fontSize: 25}}>Your password is: {password}</Text>
+            <Text style={{fontSize: 25}}>Your email is: {email}</Text>
+          </>
+        ) : null}
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   heading: {
-    textAlign:'center',
-    fontSize:40,
-    color:'blue'
-  },
-  changeHeading: {
-    marginLeft:20,
-    fontSize:20,
-    color:'red'
+    textAlign: 'center',
+    fontSize: 40,
+    color: 'blue',
   },
   textInput: {
-  color:'red',
-  borderWidth:2,
-  margin:20,
-  fontSize:20,
-  borderColor:'blue'
-  }
-})
+    color: 'red',
+    borderWidth: 2,
+    marginTop: 10,
+    marginLeft: 20,
+    marginRight: 20,
+    fontSize: 20,
+    borderColor: 'blue',
+  },
+});
 
 export default App;
